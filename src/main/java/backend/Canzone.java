c29754d45d63c8a751779873a3c1002b1d23e5ee
lucaacquati <luca.acquati10@gmail.com>
2023-05-10 17:34:41 +0200

    DatabaseConnection

---
+++ src/main/java/backend/Canzone.java
@@ -0,0 +1,28 @@
+package backend;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+
+public class Canzone {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int anno;
+    private String codice;
+    private String artista;
+    private String titolo;
+
+}
