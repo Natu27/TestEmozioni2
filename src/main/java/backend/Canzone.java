e3418e60c116e4df71fb36bcf34f39864d5826c0
lucaacquati <luca.acquati10@gmail.com>
2023-05-11 16:20:59 +0200

    DatabaseConnection

--- src/main/java/backend/Canzone.java
+++ src/main/java/backend/Canzone.java
@@ -1,28 +1,26 @@
 package backend;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @Data
 @EqualsAndHashCode
 @NoArgsConstructor
+@Getter
+@Setter
 @Entity
+@Table(name = "public.\"Canzoni\"")
+
 public class Canzone {
     @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private int anno;
 
-    private String codice;
+    //private String codice;
 
     private String artista;
 
--- src/main/java/backend/CanzoneService.java
+++ src/main/java/backend/CanzoneService.java
@@ -1,8 +1,11 @@
 package backend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import java.sql.*;
 import java.util.List;
 
 @Service
@@ -10,7 +13,30 @@ public class CanzoneService {
     @Autowired
     CanzoneRepository songRepository;
 
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    private final String tabella = "\"Canzoni\"";
     public List<Canzone> findAll() {
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement stmt = conn.createStatement();
+            String query = "SELECT * FROM public." + tabella;
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                int anno = rs.getInt("anno");
+                String autore = rs.getString("autore");
+                String codice = rs.getString("codice");
+                String titolo = rs.getString("titolo");
+                //System.out.println("Anno: " + anno + "\n" + "Autore: " + autore + "\n" + "Titolo: " + titolo + "\n" + "------------------------");
+            }
+        }catch(
+    SQLException e){
+            e.printStackTrace();
+    }
         return songRepository.findAll();
     }
 
