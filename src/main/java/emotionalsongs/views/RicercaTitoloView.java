1a0df5fc247a7c400e0fbd662d0ee00c7e7795fa
lucaacquati <luca.acquati10@gmail.com>
2023-05-20 10:18:17 +0200

    Aggiunta lista anni

--- src/main/java/emotionalsongs/views/RicercaTitoloView.java
+++ src/main/java/emotionalsongs/views/RicercaTitoloView.java
@@ -3,6 +3,7 @@ package emotionalsongs.views;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.icon.Icon;
@@ -22,6 +23,7 @@ import emotionalsongs.backend.Servizi;
 import emotionalsongs.backend.entities.Canzone;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.List;
 
 @PageTitle("Ricerca")
@@ -34,8 +36,9 @@ public class RicercaTitoloView extends VerticalLayout {
     H3 titoloPagina;
     TextField titoloDaCercare;
     TextField autoreDaCercare;
-    TextField annoDaCercare;
-    //ComboBox<Integer> annoDaCercare;
+    //TextField annoDaCercare;
+    ComboBox<Integer> annoDaCercare;
+    List<Integer> anni;
     Button searchButton;
     Grid<Canzone> grid = new Grid<>(Canzone.class);
     List<Canzone> result;
@@ -60,10 +63,12 @@ public class RicercaTitoloView extends VerticalLayout {
 
         titoloDaCercare = new TextField();
         autoreDaCercare = new TextField();
-        annoDaCercare = new TextField();
+        annoDaCercare = new ComboBox<Integer>();
         titoloDaCercare.setPlaceholder("Inserisci titolo...");
         autoreDaCercare.setPlaceholder("Inserisci autore...");
         annoDaCercare.setPlaceholder("Inserisci anno...");
+        anni = generaAnni(1922, 2023);
+        annoDaCercare.setItems(anni);
         searchButton = new Button("Cerca", buttonClickEvent -> {
             search();
         });
@@ -92,7 +97,7 @@ public class RicercaTitoloView extends VerticalLayout {
         try {
             //TO TRY - da provare con Eccezione metodo searchSong() da catchare e gestire vari casi...
             result = stub.searchSong(titoloDaCercare.getValue(), autoreDaCercare.getValue(),
-                    Integer.getInteger(annoDaCercare.getValue()));
+                    Integer.getInteger(String.valueOf(annoDaCercare.getValue())));
             grid.setItems(result);
             //Per memorizzare la grid corrente
             VaadinSession.getCurrent().setAttribute(List.class, result);
@@ -104,4 +109,12 @@ public class RicercaTitoloView extends VerticalLayout {
             e.printStackTrace();
         }
     }
+    private List<Integer> generaAnni(int annoInizio, int annoFine) {
+        List<Integer> anni = new ArrayList<>();
+        for (int i = annoInizio; i <= annoFine; i++) {
+            anni.add(i);
+        }
+        return anni;
+    }
+
 }
